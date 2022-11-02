69a7b8d28e6fabcbd7a99bc26de0148c38389046
Nicole <nicole.georgievva@gmail.com>
2022-11-02 19:22:07 +0200

    Refactor info section and add content (WIP)

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -6,21 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,6 +42,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -61,35 +65,68 @@ fun CreateBizCard() {
             ) {
                 CreateProfilePicture()
                 Divider()
-                Column() {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = "Nicole G.",
-                            modifier = Modifier.padding(top = 8.dp),
-                            color = Color.Blue,
-                            fontSize = 40.sp,
-                            fontWeight = FontWeight.Bold,
-                            letterSpacing = 2.sp,
-                            textAlign = TextAlign.Left
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(
-                            text = "+",
-                            color = Color.DarkGray,
-                            fontSize = 28.sp
-                        )
-                    }
+                CreateInfo()
+                Button(onClick = {
+
+                }) {
                     Text(
-                        text = "Junior Android Developer",
-                        modifier = Modifier,
-                        textAlign = TextAlign.Left
+                        text = "Portfolio", style = MaterialTheme.typography.button
                     )
-                    Text(text = "@nicolegeorgieva")
                 }
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxSize(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(
+                data = listOf(
+                    "Project 1", "Project 2", "Project 3"
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Text(item)
+        }
+    }
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Nicole G.",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primaryVariant,
+        )
+        Text(
+            text = "Junior Android Developer", modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            text = "@nicolegeorgieva",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
     }
 }
 
@@ -114,7 +151,7 @@ private fun CreateProfilePicture(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetBizCardTheme {
